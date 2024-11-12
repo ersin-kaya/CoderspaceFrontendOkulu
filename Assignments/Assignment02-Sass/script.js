cfa31ad639d38860cbe07e77a44bd9259b05b236
ersin <ersin-kaya@outlook.com.tr>
2024-11-13 01:22:43 +0300

    Assignment02-Sass: Added dark theme feature and applied to navbar

--- Assignments/Assignment02-Sass/script.js
+++ Assignments/Assignment02-Sass/script.js
@@ -1,9 +1,35 @@
+// Theme
+const setClassAndDataTheme = (newTheme) => {
+  document.body.classList.remove("theme-dark", "theme-light");
+  document.body.classList.add(`theme-${newTheme}`);
+  document.body.setAttribute("data-theme", newTheme);
+};
+
+const toggleTheme = () => {
+  const currentTheme = document.body.classList.contains("theme-dark")
+    ? "dark"
+    : "light";
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  setClassAndDataTheme(newTheme);
+  localStorage.setItem("theme", newTheme);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  const theme = savedTheme || "light";
+
+  setClassAndDataTheme(theme);
+});
+
+// Menu
 function toggleMenu() {
   const menuContent = document.getElementById("menu-content");
   menuContent.classList.toggle("collapsed");
   menuContent.classList.toggle("expanded");
 }
 
+// Card
 window.addEventListener("load", adjustAllBackgroundHeights);
 window.addEventListener("resize", adjustAllBackgroundHeights);
 
