d795a6c476092230513b2dcc0d51511fe42f17f4
ersin <ersin-kaya@outlook.com.tr>
2024-11-23 13:44:21 +0300

    Styled Components: MyButton

--- Lessons/Lesson07-Styled-Components/styled-components/src/App.jsx
+++ Lessons/Lesson07-Styled-Components/styled-components/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const MyButton = styled.button`
+    background-color: orangered;
+    border-radius: 10x;
+    color: white;
+    padding: 5px 20px;
+    margin: 0 20px;
+  `;
 
   return (
     <>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
+
+      <MyButton>Styled Button</MyButton>
     </>
   );
 }
