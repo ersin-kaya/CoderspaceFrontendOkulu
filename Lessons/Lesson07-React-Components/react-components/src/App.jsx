e5422bab7df423c62bae5718fc2db4e6ec532ffa
ersin <ersin-kaya@outlook.com.tr>
2024-11-23 11:53:24 +0300

    useState, useEffect

--- Lessons/Lesson07-React-Components/react-components/src/App.jsx
+++ Lessons/Lesson07-React-Components/react-components/src/App.jsx
@@ -1,16 +1,34 @@
 import ListItem from "./components/ListItem";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
   const students = ["Ali", "Mehmet", "Hakan"];
+
+  useEffect(() => {
+    console.log("Component loaded");
+  }, []);
+
+  useEffect(() => {
+    console.log("Button clicked");
+  }, [value]);
+
   return (
     <>
       <h1>Welcome</h1>
       <ul>
         {students.map((student, index) => {
-          return <ListItem key={index} student={student} />;
+          return <ListItem key={index} student={student} value={value} />;
         })}
       </ul>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Increase
+      </button>
     </>
   );
 }
--- Lessons/Lesson07-React-Components/react-components/src/components/ListItem.jsx
+++ Lessons/Lesson07-React-Components/react-components/src/components/ListItem.jsx
@@ -1,11 +1,25 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 
-function ListItem(props) {
-  return <li>{props.student}</li>;
+function ListItem({ student, value }) {
+  //   useEffect(() => {
+  //     console.log("Test useEffect"); // komponent her çağrıldığında bir kez çalışıyor. StrictMode'u kapatmayı unutma!
+  //   });
+  useEffect(() => {
+    console.log("Test useEffect"); // komponent her çağrıldığında bir kez çalışıyor.
+    // ! StrictMode açık olduğunda geliştirme modunda iki kez çalışır
+  }, []); // bir kez çalışması için boş bir dependency list ekliyoruz
+
+  return (
+    <li>
+      {student} - {value}
+    </li>
+  );
 }
 
 ListItem.propTypes = {
   student: PropTypes.string.isRequired,
+  value: PropTypes.number,
 };
 
 export default ListItem;
