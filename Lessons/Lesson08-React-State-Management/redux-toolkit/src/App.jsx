7a229958dd85db46e0280c34d9287dc46d79c74e
ersin <ersin-kaya@outlook.com.tr>
2024-11-24 11:23:32 +0300

    State Management: Redux Toolkit - Todo App

--- Lessons/Lesson08-React-State-Management/redux-toolkit/src/App.jsx
+++ Lessons/Lesson08-React-State-Management/redux-toolkit/src/App.jsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, deleteTodo } from "./redux/slices/todoSlice";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
 
-  return <></>;
+  const handleAdd = (todo) => {
+    dispatch(addTodo(todo));
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <>
+      <h1>Redux Toolkit - Todo App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const todo = e.target[0].value;
+          handleAdd({ id: Date.now(), title: todo, completed: false });
+        }}
+      >
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => handleDelete(todo.id)}>x</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
