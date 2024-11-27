4476e386681cb321084212ab5abf6c9335483ccc
ersin <ersin-kaya@outlook.com.tr>
2024-11-27 21:23:12 +0300

    React Router: Created menu to use NavLink for navigation

--- Lessons/Lesson07-React-Router/react-router/src/App.jsx
+++ Lessons/Lesson07-React-Router/react-router/src/App.jsx
@@ -1,10 +1,21 @@
 import Router from "./routes/Router";
 import "./App.css";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="menu">Menu</div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/parameter/10">Parameter (e.g., 10)</NavLink>
+        </li>
+      </ul>
       <Router />
     </>
   );
