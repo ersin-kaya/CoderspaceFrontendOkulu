97a7b9221d4039ede974573eb719fb023e92129d
ersin <ersin-kaya@outlook.com.tr>
2024-11-27 21:16:07 +0300

    React Router: Displayed route parameters using useParams()

--- Lessons/Lesson07-React-Router/react-router/src/routes/Router.jsx
+++ Lessons/Lesson07-React-Router/react-router/src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import AboutView from "../views/AboutView";
 import HomeView from "../views/HomeView";
 import { useRoutes } from "react-router-dom";
+import ParameterView from "../views/ParameterView";
 
 const Router = () => {
   const routes = useRoutes([
     { path: "/", element: <HomeView /> },
     { path: "/about", element: <AboutView /> },
+    { path: "/parameter/:id", element: <ParameterView /> },
   ]);
 
   return routes;
